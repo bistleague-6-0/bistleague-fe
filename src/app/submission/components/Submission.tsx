5dcc00b0c09210c4b0a34f5e4b8e6fa826b3195a
Abraham Megantoro Samudra <18221123@mahasiswa.itb.ac.id>
2023-10-30 15:31:17 +0700

    Fix submission

--- src/app/submission/components/Submission.tsx
+++ src/app/submission/components/Submission.tsx
@@ -6,42 +6,10 @@ import axios from "axios";
 import Image from "next/image";
 
 import BulatKiri from "@images/submission/left.svg";
+import Cookies from "universal-cookie";
 import Link from "next/link";
-
-interface TeamMember {
-  user_id: string;
-  username: string;
-  fullname: string;
-  is_leader: boolean;
-}
-
-interface TeamData {
-  team_id: string;
-  team_name: string;
-  team_redeem_code: string;
-  is_active: boolean;
-  payment_proof: string;
-  payment_proof_url: string;
-  payment_status: string;
-  payment_status_code: number;
-  student_card: string;
-  student_card_url: string;
-  student_card_status: string;
-  student_card_status_code: number;
-  self_portrait: string;
-  self_portrait_url: string;
-  self_portrait_status: string;
-  self_portrait_status_code: number;
-  twibbon: string;
-  twibbon_url: string;
-  twibbon_status: string;
-  twibbon_status_code: number;
-  enrollment: string;
-  enrollment_url: string;
-  enrollment_status: string;
-  enrollment_status_code: number;
-  members: TeamMember[];
-}
+import { useRouter } from 'next/navigation'
+import toast from "react-hot-toast";
 
 interface SubmittedData {
   doc_type: string;
@@ -58,18 +26,26 @@ interface SubmissionData {
 }
 
 interface SubmissionProps {
-  teamData: TeamData;
   submissionData: SubmissionData;
   jwt_token: string;
+  refresh: string;
+  user_id: string
 }
 
 export default function Submission({
-  teamData,
   submissionData,
   jwt_token,
+  refresh,
+  user_id
 }: SubmissionProps) {
+  const router = useRouter();
+  const cookie = new Cookies();
   const [selectedFile, setSelectedFile] = useState("No file selected");
   const [base64, setBase64] = useState("");
+  const BASE_URL =
+    process.env.NEXT_PUBLIC_STAGE != "staging"
+      ? "https://be-production-b6utdt2kwa-et.a.run.app"
+      : "https://be-staging-b6utdt2kwa-et.a.run.app";
 
   const fileChangedHandler = async (e: React.ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) return;
@@ -92,10 +68,6 @@ export default function Submission({
     };
 
     try {
-      const BASE_URL =
-        process.env.NEXT_PUBLIC_STAGE != "staging"
-          ? "https://be-production-b6utdt2kwa-et.a.run.app"
-          : "https://be-staging-b6utdt2kwa-et.a.run.app";
       const submit = await axios.post(
         BASE_URL + "/team/document",
         submittedData,
@@ -105,20 +77,34 @@ export default function Submission({
           },
         }
       );
+
+      router.refresh()
+      toast.success('Submit successfull');
       setSelectedFile("No file selected");
-    } catch (err) {
-      console.log(err);
+    } catch (error) {
+      const errorResponse = error as {
+        response: { status: number; data: { message: string } };
+      };
+      if (errorResponse.response.status === 401) {
+        const response2 = await axios.post(BASE_URL + "/refresh", {
+          refresh_token: refresh,
+          user_id: user_id,
+        });
+        cookie.remove("jwt_token", { path: "/" });
+        cookie.set("jwt_token", response2.data.data.jwt_token, { path: "/" });
+        router.refresh()
+      }
     }
   };
   return (
     <div className="px-5 py-7">
       <div className="flex text-[#F3EEE7] items-center justify-between mt-8 lg:mt-0 relative z-10">
-        <div className="flex items-center gap-3">
+        <Link href="/competition" className="flex items-center gap-3">
           <AiOutlineArrowLeft className="text-2xl lg:text-base" />
           <h1 className="hidden lg:inline font-bold text-base">
             Back to Competition
           </h1>
-        </div>
+        </Link >
         <h1 className="w-full self-center font-extrabold text-2xl text-center lg:hidden">
           Submission
         </h1>
@@ -153,52 +139,50 @@ export default function Submission({
             </div>
           )}
 
-          <form onSubmit={submitFile}>
-            <div className="border border-[#BDBDBD] mb-4 lg:mx-16 p-3.5 rounded text-sm font-medium flex items-center justify-between">
-              <Link
-                href={submissionData.submission_url}
-                className={
-                  !submissionData ? "text-[#BDBDBD]" : "text-[#413687]"
-                }
-              >
-                {submissionData ? submissionData.submission_filename : "Empty"}
-              </Link>
-              <AiFillCheckCircle
-                className={
-                  !submissionData
-                    ? "hidden"
-                    : "inline-block ml-2 text-[#27ae60] w-6 h-6"
-                }
-              />
-            </div>
-            <div className="flex flex-row items-center border lg:mx-16 mb-6 border-[#BDBDBD] stroke-1 rounded justify-between pr-1 pl-2.5 py-1.5">
-              <label className="text-sm text-[#4F4F4F] font-medium">
-                {selectedFile}
-              </label>
-              <input
-                type="file"
-                id="custom-input"
-                onChange={fileChangedHandler}
-                hidden
-              />
-              <label
-                htmlFor="custom-input"
-                className="block text-white ml-4 py-2.5 px-5 rounded-md border-0 text-sm font-semibold bg-[#413687] cursor-pointer"
-              >
-                {!submissionData ? "Upload" : "Reupload"}
-              </label>
-            </div>
-            <div className="mb-4 lg:mx-16 rounded text-sm font-bold flex items-center">
-              <button
-                type="submit"
-                className="bg-[#413687] px-12 py-3 lg:px-24 rounded-lg cursor-pointer w-full"
-              >
-                <h1 className="text-white text-center font-bold lg:text-xl">
-                  Submit
-                </h1>
-              </button>
-            </div>
-          </form>
+          <div className="border border-[#BDBDBD] mb-4 lg:mx-16 p-3.5 rounded text-sm font-medium flex items-center justify-between">
+            <Link
+              href={submissionData?.submission_url || ""}
+              className={
+                !submissionData ? "text-[#BDBDBD]" : "text-[#413687]"
+              }
+            >
+              {submissionData ? submissionData.submission_filename : "Empty"}
+            </Link>
+            <AiFillCheckCircle
+              className={
+                !submissionData
+                  ? "hidden"
+                  : "inline-block ml-2 text-[#27ae60] w-6 h-6"
+              }
+            />
+          </div>
+          <div className="flex flex-row items-center border lg:mx-16 mb-6 border-[#BDBDBD] stroke-1 rounded justify-between pr-1 pl-2.5 py-1.5">
+            <label className="text-sm text-[#4F4F4F] font-medium">
+              {selectedFile}
+            </label>
+            <input
+              type="file"
+              id="custom-input"
+              onChange={fileChangedHandler}
+              hidden
+            />
+            <label
+              htmlFor="custom-input"
+              className="block text-white ml-4 py-2.5 px-5 rounded-md border-0 text-sm font-semibold bg-[#413687] cursor-pointer"
+            >
+              {!submissionData ? "Upload" : "Reupload"}
+            </label>
+          </div>
+          <div className="mb-4 lg:mx-16 rounded text-sm font-bold flex items-center">
+            <button
+              type="submit"
+              className="bg-[#413687] px-12 py-3 lg:px-24 rounded-lg cursor-pointer w-full"
+            >
+              <h1 className="text-white text-center font-bold lg:text-xl" onClick={submitFile}>
+                Submit
+              </h1>
+            </button>
+          </div>
         </div>
       </div>
       <Image
--- src/app/submission/page.tsx
+++ src/app/submission/page.tsx
@@ -2,12 +2,17 @@ import Submission from "./components/Submission";
 
 import axios from "axios";
 import { get } from "http";
+import Cookies from "universal-cookie";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default async function SubmissionPage() {
+  const cookie = new Cookies();
   const cookieStore = cookies();
   const jwt_token = cookieStore.get("jwt_token")?.value as string;
+  const refresh = cookieStore.get("refresh")?.value as string;
+  const user_id = cookieStore.get("user_id")?.value as string;
+  
   const BASE_URL =
     process.env.NEXT_PUBLIC_STAGE != "staging"
       ? "https://be-production-b6utdt2kwa-et.a.run.app"
@@ -27,20 +32,6 @@ export default async function SubmissionPage() {
     return date.toLocaleString("en-US", options);
   };
 
-  const getTeam = async () => {
-    try {
-      const response = await axios.get(BASE_URL + "/team", {
-        headers: {
-          Authorization: `Bearer ${jwt_token}`,
-        },
-      });
-
-      return response.data.data;
-    } catch (error) {
-      console.error("Error getting profile:", error);
-    }
-  };
-
   const getLastSubmission = async () => {
     try {
       const response = await axios.get(BASE_URL + "/team/submission/1", {
@@ -55,20 +46,30 @@ export default async function SubmissionPage() {
       response.data.data.submission_lastupdate = formattedDate;
       return response.data.data;
     } catch (error) {
-      return null;
+      // const errorResponse = error as {
+      //   response: { status: number; data: { message: string } };
+      // };
+      // if (errorResponse?.response?.status === 401) {
+      //   const response2 = await axios.post(BASE_URL + "/refresh", {
+      //     refresh_token: refresh,
+      //     user_id: user_id,
+      //   });
+      //   cookie.remove("jwt_token", { path: "/" });
+      //   cookie.set("jwt_token", response2.data.data.jwt_token, { path: "/" });
+      // }
+      // console.log("error2",errorResponse?.response?.status)
     }
   };
 
-  const teamData = await getTeam();
   const submissionData = await getLastSubmission();
-
   return (
     <div className="bg-[url('/images/compregister/bg-comp-mobile.svg')] lg:bg-[url('/images/compregister/bg-comp-web.svg')] bg-cover min-h-screen flex flex-col">
       <div className="bg-[url('/images/compregister/right.svg')] lg:bg-[url('/images/compregister/right-web.svg')] bg-no-repeat bg-right-bottom lg:bg-right-top flex-1">
         <Submission
-          teamData={teamData}
           jwt_token={jwt_token}
           submissionData={submissionData}
+          refresh={refresh}
+          user_id={user_id}
         />
       </div>
     </div>
